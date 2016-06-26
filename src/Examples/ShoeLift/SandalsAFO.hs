fa96ef449e6dab5b08d3e2ff52969250a0a8e391
Heath Weiss <heathweiss@hotmail.com>
2016-06-26 08:59:01 -0600

    lifts are printed

--- src/Examples/ShoeLift/SandalsAFO.hs
+++ src/Examples/ShoeLift/SandalsAFO.hs
@@ -7,6 +7,9 @@ import CornerPoints.FaceConversions(upperFaceFromLowerFace, backFaceFromFrontFac
 import CornerPoints.FaceExtraction(extractFrontFace, extractTopFace, extractBottomFace, extractRightFace)
 import CornerPoints.Transpose(transposeX, transposeY, transposeZ)
 import CornerPoints.MeshGeneration(autoGenerateEachCube, autoGenerateEachFace)
+import CornerPoints.Rotations(rotateAlongYaxis90, rotateAlongYaxis180)
+import CornerPoints.Slicer(slice)
+
 import Stl.StlBase(Triangle(..), newStlShape)
 import Stl.StlCornerPoints((|+++^|), Faces(..) )
 import Stl.StlFileWriter(writeStlToFile)
@@ -24,7 +27,7 @@ import Control.Monad.Except
 import Control.Monad.Writer (WriterT, tell, execWriterT)
 import Control.Monad.Reader
 
-import CornerPoints.Slicer(slice)
+
 {--------------------------- overview-------------------------
 Built June 2016
 AFO sandals
@@ -36,22 +39,60 @@ Use cubical input system.
 Use ExceptT State builder
 
 Use Slicr module to cut and rotate, instead of doing with Slicr3r
+
+The new 3D capture system gave both sides as X cood's instead of 1 X cood and a width.
+This does not need to get converted to a width system so it works with Cubical, as I can directly use CornerPoints
+Maybe Cubical should have both systems?
+
+------------------------ printing notes:----------------------
+-----heel btm slice:
+5 % honeycomb infill
+-honeycombs are about 25 mm across.
+-should try 4 %
+1 perimeter
+no detect thin walls
+no extra perimeters if needed
+Used Slicr3r to rotate 180 degrees on y axis
+
+Arch was short as I did not include the last input from heel.
+There has to be an overlap of inputs.
+Add it in and print it again.
+
+------heel top slice
+-same as heel btm slice
+
+-----arch bottom
+-same as heel btm
+
+-----arch top
+Done.
+X width at the slice did not work out well.
+
+-----toe bottom
 -}
+
+
 
 --curry in the stack pushing function
 buildCubePointsList' = buildCubePointsList (++)
 
-runAutoGenerateEachCube inState =
-  autoGenerateEachCube [] ((execState $ runExceptT heelSandalBtmSlice ) inState)
-  
-
-generateStl =
-  let faces = runAutoGenerateEachCube []
+{-
+The heel of the sandal.
+Will slice it off and rotate for flat printing.
+-}
+----------------------------------------------- HeelSandalBtmSlice----------------------------------------
+generateHeelSandalBtmSliceStl =
+  let faces = generateHeelSandalBtmSliceFaces []
       triangles' = [FacesAll | x <- [1..]] |+++^| faces
   in
-  writeStlToFile $ newStlShape "stack of cubes" triangles'
-  
+  writeStlToFile $ newStlShape "HeelSandalBtmSlice" triangles'
 
+generateHeelSandalBtmSliceFaces inState =
+  autoGenerateEachCube [] ((execState $ runExceptT heelSandalBtmSlice ) inState)
+
+{-
+Take the bottom slice of the heel.
+-}
 heelSandalBtmSlice :: ExceptT BuilderError (State CpointsStack ) CpointsList
 heelSandalBtmSlice = do
   topFaces <- buildCubePointsList' "create top faces" ((head heelSandal) +++> (tail heelSandal)) [CornerPointsId | x <- [1..]]
@@ -63,21 +104,156 @@ heelSandalBtmSlice = do
                   ]
                   [CornerPointsId | x <- [1..]]
   btmSlices <- buildCubePointsList' "create bottom slice" slicedTops btmFaces
-  return btmSlices
-{-
-The heel of the sandal.
-Will slice it off and rotate for flat printing.
+  return  btmSlices
 
-The new 3D capture system gave both sides as X cood's instead of 1 X cood and a width.
-This gets converted to a width system so it works with Cubical.
-Maybe Cubical should have both systems?
+-------------------------------- HeelSandalTopSlice --------------------------------------------
+reportGenerateHeelSandalTopSliceFaces :: IO ()
+reportGenerateHeelSandalTopSliceFaces = do
+  print $ show $ (evalState $ runExceptT heelSandalTopSlice )  []
+
+generateHeelSandalTopSliceStl =
+  let faces = generateHeelSandalTopSliceFaces []
+      triangles' = [FacesAll | x <- [1..]] |+++^| faces
+  in
+  writeStlToFile $ newStlShape "HeelSandalBtmSlice" triangles'
+
+generateHeelSandalTopSliceFaces inState =
+  autoGenerateEachCube [] ((execState $ runExceptT heelSandalTopSlice ) inState)
+  
+{-Take the top slice of the heel-}
+heelSandalTopSlice :: ExceptT BuilderError (State CpointsStack ) CpointsList
+heelSandalTopSlice = do
+  topFaces <- buildCubePointsList' "create top faces" ((head heelSandal) +++> (tail heelSandal)) [CornerPointsId | x <- [1..]]
+  btmFaces <- buildCubePointsList' "create bottom faces" ((head treadHeel) +++> (tail treadHeel)) [CornerPointsId | x <- [1..]]
+  slicedBottoms <- buildCubePointsList' "create sliced bottom"
+                  [ slice 110 btmFace topFace
+                      | topFace <- topFaces
+                      | btmFace <- btmFaces
+                  ]
+                  [CornerPointsId | x <- [1..]]
+  topSlices <- buildCubePointsList' "create bottom slice" slicedBottoms topFaces
+  return  topSlices  
+
+
+-------------------------------------------------- arch bottom slice ---------------------
+generateArchBtmSliceStl =
+  let faces = generateArchBtmSliceFaces []
+      triangles' = [FacesAll | x <- [1..]] |+++^| faces
+  in
+  writeStlToFile $ newStlShape "HeelSandalBtmSlice" triangles'
+
+generateArchBtmSliceFaces inState =
+  autoGenerateEachCube [] ((execState $ runExceptT archBtmSlice ) inState)
+{-
+Take the bottom slice of the heel.
 -}
---BottomFrontLine (Point ) ,
+archBtmSlice :: ExceptT BuilderError (State CpointsStack ) CpointsList
+archBtmSlice = do
+  topFaces <- buildCubePointsList' "create top faces" ((head sandalArch) +++> (tail sandalArch)) [CornerPointsId | x <- [1..]]
+  btmFaces <- buildCubePointsList' "create top faces" ((head treadArch) +++> (tail treadArch)) [CornerPointsId | x <- [1..]]
+  slicedTops <- buildCubePointsList' "create sliced tops"
+                  [ slice 90 topFace btmFace
+                      | topFace <- topFaces
+                      | btmFace <- btmFaces
+                  ]
+                  [CornerPointsId | x <- [1..]]
+  btmSlices <- buildCubePointsList' "create bottom slice" slicedTops btmFaces
+  return  btmSlices
+
+----------------------------arch top slice--------------------------------------
+reportGenerateArchTopSliceFaces :: IO ()
+reportGenerateArchTopSliceFaces = do
+  print $ show $ (evalState $ runExceptT archTopSlice )  []
+
+generateArchTopSliceStl =
+  let faces = generateArchTopSliceFaces []
+      triangles' = [FacesAll | x <- [1..]] |+++^| faces
+  in
+  writeStlToFile $ newStlShape "HeelSandalBtmSlice" triangles'
+
+generateArchTopSliceFaces inState =
+  autoGenerateEachCube [] ((execState $ runExceptT archTopSlice ) inState)
+  
+{-Take the bottom slice of the arch-}
+archTopSlice :: ExceptT BuilderError (State CpointsStack ) CpointsList
+archTopSlice = do
+  topFaces <- buildCubePointsList' "create top faces" ((head sandalArch) +++> (tail sandalArch)) [CornerPointsId | x <- [1..]]
+  btmFaces <- buildCubePointsList' "create bottom faces" ((head treadArch) +++> (tail treadArch)) [CornerPointsId | x <- [1..]]
+  slicedBottoms <- buildCubePointsList' "create sliced bottom"
+                  [ slice 90 btmFace topFace
+                      | topFace <- topFaces
+                      | btmFace <- btmFaces
+                  ]
+                  [CornerPointsId | x <- [1..]]
+  topSlices <- buildCubePointsList' "create bottom slice" slicedBottoms topFaces
+  return  topSlices  
+
+
+
+------------------------------------------- toe btm slice -----------------------------
+reportGenerateToeBottomSliceFaces :: IO ()
+reportGenerateToeBottomSliceFaces = do
+  print $ show $ (evalState $ runExceptT toeBtmSlice )  []
+
+generateToeBtmSliceStl =
+  let faces = generateToeBtmSliceFaces []
+      triangles' = [FacesAll | x <- [1..]] |+++^| faces
+  in
+  writeStlToFile $ newStlShape "HeelSandalBtmSlice" triangles'
+
+generateToeBtmSliceFaces inState =
+  autoGenerateEachCube [] ((execState $ runExceptT toeBtmSlice ) inState)
+{-
+Take the bottom slice of the heel.
+-}
+toeBtmSlice :: ExceptT BuilderError (State CpointsStack ) CpointsList
+toeBtmSlice = do
+  topFaces <- buildCubePointsList' "create top faces" ((head sandalToe) +++> (tail sandalToe)) [CornerPointsId | x <- [1..]]
+  btmFaces <- buildCubePointsList' "create top faces" ((head treadToe) +++> (tail treadToe)) [CornerPointsId | x <- [1..]]
+  slicedTops <- buildCubePointsList' "create sliced tops"
+                  [ slice 75 topFace btmFace
+                      | topFace <- topFaces
+                      | btmFace <- btmFaces
+                  ]
+                  [CornerPointsId | x <- [1..]]
+  btmSlices <- buildCubePointsList' "create bottom slice" slicedTops btmFaces
+  return  btmSlices
+
+-------------------------- toe top slice ---------------------------------
+reportGenerateToeTopSliceFaces :: IO ()
+reportGenerateToeTopSliceFaces = do
+  print $ show $ (evalState $ runExceptT toeTopSlice )  []
+
+generateToeTopSliceStl =
+  let faces = generateToeTopSliceFaces []
+      triangles' = [FacesAll | x <- [1..]] |+++^| faces
+  in
+  writeStlToFile $ newStlShape "HeelSandalBtmSlice" triangles'
+
+generateToeTopSliceFaces inState =
+  autoGenerateEachCube [] ((execState $ runExceptT toeTopSlice ) inState)
+  
+{-Take the bottom slice of the arch-}
+toeTopSlice :: ExceptT BuilderError (State CpointsStack ) CpointsList
+toeTopSlice = do
+  topFaces <- buildCubePointsList' "create top faces" ((head sandalToe) +++> (tail sandalToe)) [CornerPointsId | x <- [1..]]
+  btmFaces <- buildCubePointsList' "create bottom faces" ((head treadToe) +++> (tail treadToe)) [CornerPointsId | x <- [1..]]
+  slicedBottoms <- buildCubePointsList' "create sliced bottom"
+                  [ slice 75 btmFace topFace
+                      | topFace <- topFaces
+                      | btmFace <- btmFaces
+                  ]
+                  [CornerPointsId | x <- [1..]]
+  topSlices <- buildCubePointsList' "create bottom slice" slicedBottoms topFaces
+  return  topSlices  
+
+
+
 heelSandal :: [CornerPoints]
 heelSandal =
-  [ BackTopLine (Point  40   99.8  133.7)   (Point 50   99.9  133.7),
+  [ BackTopLine (Point  40   99.8  133.7)   (Point 50   99.8  133.7),
     FrontTopLine (Point 40   99.9  133.7)   (Point 50   99.9  133.7),
-    FrontTopLine (Point 40   100   133.7)   (Point 50   99.9  133.7),
+    FrontTopLine (Point 40   100   133.7)   (Point 50   100  133.7),
     FrontTopLine (Point 35   102   133.5)   (Point 60   102   133.5),
     FrontTopLine (Point 32   105   133.1)   (Point 65.3 105   133.1),
     FrontTopLine (Point 24   110   133)     (Point 71   110   133), 
@@ -88,43 +264,9 @@ heelSandal =
     FrontTopLine (Point 13.5 140   125.8)   (Point 84   140   125.8),
     FrontTopLine (Point 13   150   123.7)   (Point 85   150   123.7),
     FrontTopLine (Point 11.5 160   122.4)   (Point 85   160   122.4),
-    FrontTopLine (Point 11   170   119.7)   (Point 86   170   119.7)
+    FrontTopLine (Point 11   170   119.7)   (Point 86   170   119.7),
+    FrontTopLine  (Point 11   180   117.2)   (Point 87   180   117.2)
   ]
-
-sandalArch :: [CornerPoints]
-sandalArch =
-  [
-    BackTopLine  (Point 11   180   117.2)   (Point 87   180   117.2),
-    FrontTopLine (Point 11   190   115.8)   (Point 87.5 190   115.8),
-    FrontTopLine (Point 10.5 200   114.7)   (Point 87   200   114.7),
-    FrontTopLine (Point 8.5  210   111.7)   (Point 86.5 210   111.7),
-    FrontTopLine (Point 7    220   109.9)   (Point 89   220   109.9),
-    FrontTopLine (Point 3.5  230   107.7)   (Point 90   230   107.7),
-    FrontTopLine (Point 2.5  240   104.8)   (Point 91   240   104.8),
-    FrontTopLine (Point 2.5  250   103.4)   (Point 91   250   103.4),
-    FrontTopLine (Point 1    260   101.4)   (Point 90.5 260   101.4)
-  ]
-
-sandalToe :: [CornerPoints]
-sandalToe =
-  [ 
-    BackTopLine (Point 1.5  270   100)     (Point 90.5 270   100),
-    FrontTopLine (Point 3.5  280   99.6)    (Point 91   280   99.6),
-    FrontTopLine (Point 3.5  290   98.3)    (Point 89   290   98.3),
-    FrontTopLine (Point 5.8  300   98.1)    (Point 83.5 300   98.1),
-    FrontTopLine (Point 6.3  305   97.3)    (Point 79.5 305   97.3),
-    FrontTopLine (Point 12.5 310   97.3)    (Point 78.5 310   97.3),
-    FrontTopLine (Point 16   315   97.3)    (Point 77.5 315   97.3),
-    FrontTopLine (Point 20   320   97.2)    (Point 73.5 320   97.2),
-    FrontTopLine (Point 22.5 325   97.3)    (Point 68.5 325   97.3),
-    FrontTopLine (Point 30   330   96.9)    (Point 63.5 330   96.9),
-    FrontTopLine (Point 30.1 333   96.5)    (Point 60   333   96.5),
-    FrontTopLine (Point 30.2 335   95.7)    (Point 56   335   95.7),
-    FrontTopLine (Point 30   338   95.7)    (Point 41   338   95.7),
-    FrontTopLine (Point 30   338.1 95.7)    (Point 41   338.1 95.7), 
-    FrontTopLine (Point 30   338.2 95.7)    (Point 41   338.2 95.7)
-  ]
-
 
 treadHeel :: [CornerPoints]
 treadHeel =
@@ -142,8 +284,24 @@ treadHeel =
     BottomFrontLine (Point 15   140 94.1)  (Point 82.5 140 94.1),
     BottomFrontLine (Point 15   150 91.9)  (Point 82.5 150 91.9),
     BottomFrontLine (Point 16   160 86.8)  (Point 82.5 160 86.8),
-    BottomFrontLine (Point 16   170 84.8)  (Point 82.5 170 84.8)
+    BottomFrontLine (Point 16   170 84.8)  (Point 82.5 170 84.8),
+    BottomFrontLine  (Point 17   180   79)   (Point 81   180  79)
   ]
+
+sandalArch :: [CornerPoints]
+sandalArch =
+  [
+    BackTopLine  (Point 11   180   117.2)   (Point 87   180   117.2),
+    FrontTopLine (Point 11   190   115.8)   (Point 87.5 190   115.8),
+    FrontTopLine (Point 10.5 200   114.7)   (Point 87   200   114.7),
+    FrontTopLine (Point 8.5  210   111.7)   (Point 86.5 210   111.7),
+    FrontTopLine (Point 7    220   109.9)   (Point 89   220   109.9),
+    FrontTopLine (Point 3.5  230   107.7)   (Point 90   230   107.7),
+    FrontTopLine (Point 2.5  240   104.8)   (Point 91   240   104.8),
+    FrontTopLine (Point 2.5  250   103.4)   (Point 91   250   103.4),
+    FrontTopLine (Point 1    260   101.4)   (Point 90.5 260   101.4)
+  ]
+
 
 treadArch :: [CornerPoints]
 treadArch =
@@ -159,10 +317,33 @@ treadArch =
     BottomFrontLine (Point 7    260   44)   (Point 90   260  44)
   ]
 
+sandalToe :: [CornerPoints]
+sandalToe =
+  [ BackTopLine  (Point 1    260   101.4)   (Point 90.5 260   101.4),
+    FrontTopLine (Point 2.5  270   100)     (Point 90.5 270   100),
+    FrontTopLine (Point 2.5  280   99.6)    (Point 91   280   99.6),
+    FrontTopLine (Point 1  290   98.3)      (Point 89   290   98.3),
+    FrontTopLine (Point 1.8  300   98.1)    (Point 83.5 300   98.1),
+    FrontTopLine (Point 3.5  305   97.3)    (Point 79.5 305   97.3),
+    FrontTopLine (Point 3.5 310   97.3)     (Point 78.5 310   97.3),
+    FrontTopLine (Point 5   315   97.3)     (Point 77.5 315   97.3),
+    FrontTopLine (Point 8   320   97.2)     (Point 73.5 320   97.2),
+    FrontTopLine (Point 14 325   97.3)      (Point 68.5 325   97.3),
+    FrontTopLine (Point 19   330   96.9)    (Point 63.5 330   96.9),
+    FrontTopLine (Point 28 333   96.5)      (Point 60   333   96.5),
+    FrontTopLine (Point 31 335   95.7)      (Point 56   335   95.7),--test: +100
+    FrontTopLine (Point 34   338   95.7)    (Point 41   338   95.7),
+    FrontTopLine (Point 37   338.1 95.7)    (Point 41   338.1 95.7), 
+    FrontTopLine (Point 40   338.2 95.7)    (Point 41   338.2 95.7)
+  ]
+
+
+
 treadToe :: [CornerPoints]
 treadToe =
-  [ 
-    BackBottomLine  (Point 5    270   44.9) (Point 90   270 44.9),
+  [
+    BackBottomLine  (Point 7    260   44)   (Point 90   260  44),
+    BottomFrontLine (Point 5    270   44.9) (Point 90   270 44.9),
     BottomFrontLine (Point 4    280   45.3) (Point 90   280 45.3),
     BottomFrontLine (Point 3.5  290   46.6) (Point 88.8 290 46.6),
     BottomFrontLine (Point 4    300   48.8) (Point 87.5 300 48.8),
