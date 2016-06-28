c219e9ee1c53873dd94b74b1f24bcf3152205be0
Heath Weiss <heathweiss@hotmail.com>
2016-06-27 20:59:15 -0600

    started to use the Stack test system

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import BuilderMonadTest(builderMonadTest)
+import SlicerTest (splicerTestDo)
+
+--main :: IO ()
+main = do
+  --splicerTestDo
+  --putStrLn "Test suite not yet implemented"
+  splicerTestDo
+  builderMonadTest
