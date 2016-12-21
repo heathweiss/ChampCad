d7f47baed514ce39e95edecc1af238d2b9b2c865
Heath Weiss <heathweiss@hotmail.com>
2016-12-21 08:59:34 -0700

    updated for a extraction/conversion that was wrong.

--- test/FaceExtractAndConvertTest.hs
+++ test/FaceExtractAndConvertTest.hs
@@ -17,7 +17,13 @@ faceExtractAndConvertTestDo = do
   --Gets the right face of the large cube, as the back face.
   let getBackFaceFromRightFaceFromCubeTest = TestCase $ assertEqual
        "getBackFaceFromRightFaceFromCubeTest"
-       ( BackFace {b1 = Point {x_axis = 4.0, y_axis = 4.0, z_axis = 4.0}, b2 = Point {x_axis = 3.0, y_axis = 3.0, z_axis = 3.0}, b3 = Point {x_axis = 13.0, y_axis = 13.0, z_axis = 13.0}, b4 = Point {x_axis = 14.0, y_axis = 14.0, z_axis = 14.0}})
+       (
+         BackFace {b1 = Point {x_axis = 14.0, y_axis = 14.0, z_axis = 14.0},
+                   b2 = Point {x_axis = 13.0, y_axis = 13.0, z_axis = 13.0},
+                   b3 = Point {x_axis = 3.0, y_axis = 3.0, z_axis = 3.0},
+                   b4 = Point {x_axis = 4.0, y_axis = 4.0, z_axis = 4.0}}
+
+       )
        (toBackFace $ extractRightFace (CubePoints (Point 1 1 1) (Point 2 2 2) (Point 3 3 3) (Point 4 4 4) (Point 11 11 11) (Point 12 12 12) (Point 13 13 13) (Point 14 14 14)))
   runTestTT getBackFaceFromRightFaceFromCubeTest
 
@@ -30,7 +36,17 @@ faceExtractAndConvertTestDo = do
 
   let addPerpendicularCubesTest = TestCase $ assertEqual
        ("addPerpendicularCubesTest")
-       (CubePoints {f1 = Point {x_axis = 101.0, y_axis = 101.0, z_axis = 101.0}, f2 = Point {x_axis = 102.0, y_axis = 102.0, z_axis = 102.0}, f3 = Point {x_axis = 103.0, y_axis = 103.0, z_axis = 103.0}, f4 = Point {x_axis = 104.0, y_axis = 104.0, z_axis = 104.0}, b1 = Point {x_axis = 4.0, y_axis = 4.0, z_axis = 4.0}, b2 = Point {x_axis = 3.0, y_axis = 3.0, z_axis = 3.0}, b3 = Point {x_axis = 13.0, y_axis = 13.0, z_axis = 13.0}, b4 = Point {x_axis = 14.0, y_axis = 14.0, z_axis = 14.0}} )
+       (
+         CubePoints {f1 = Point {x_axis = 101.0, y_axis = 101.0, z_axis = 101.0},
+                     f2 = Point {x_axis = 102.0, y_axis = 102.0, z_axis = 102.0},
+                     f3 = Point {x_axis = 103.0, y_axis = 103.0, z_axis = 103.0},
+                     f4 = Point {x_axis = 104.0, y_axis = 104.0, z_axis = 104.0},
+                     b1 = Point {x_axis = 14.0, y_axis = 14.0, z_axis = 14.0},
+                     b2 = Point {x_axis = 13.0, y_axis = 13.0, z_axis = 13.0},
+                     b3 = Point {x_axis = 3.0, y_axis = 3.0, z_axis = 3.0},
+                     b4 = Point {x_axis = 4.0, y_axis = 4.0, z_axis = 4.0}}
+
+       )
        (let smallCube = (CubePoints (Point 101 101 101) (Point 102 102 102) (Point 103 103 103) (Point 104 104 104) (Point 201 2011 201) (Point 202 202 202) (Point 203 203 203) (Point 204 204 204))
             largeCube = (CubePoints (Point 1 1 1) (Point 2 2 2) (Point 3 3 3) (Point 4 4 4) (Point 11 11 11) (Point 12 12 12) (Point 13 13 13) (Point 14 14 14))
             smallInvertedFrontFace = {-reverseNormal $-} extractFrontFace smallCube
