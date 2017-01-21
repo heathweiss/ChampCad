e237a92885d1ba73f2a78d9fa155b6c4c8234cd1
Heath Weiss <heathweiss@hotmail.com>
2017-01-21 07:41:27 -0700

    added in the AngleRadius functionality

--- src/CornerPoints/Create.hs
+++ src/CornerPoints/Create.hs
@@ -9,7 +9,10 @@ module CornerPoints.Create(
   Origin(..),
   Angle(..),
   createCornerPointSquaredOff,
-  getQuadrantAngle
+  getQuadrantAngle,
+  AngleRadius(..),
+  extractAngles,
+  extractRadii
   ) where
 import CornerPoints.Points(Point(..))
 import CornerPoints.CornerPoints(CornerPoints(..), (+++), )
@@ -62,6 +65,9 @@ orign: Point
 -The starting point which gets adjusted to give the return point. That way, this point can be created relative to some position, instead of at an origin of 0 0 0
 
 -}
+
+type Power = Double
+
 
 createCornerPoint :: (Point-> CornerPoints) -> Origin -> Radius ->  Angle -> Slope -> Slope -> CornerPoints
 createCornerPoint cPoint origin horizRadius verticalAngle xSlope ySlope  =
@@ -497,4 +503,51 @@ adjustRadiusForSlope (Radius radius) (NegSlope xySlope) = DownRadius $ radius * 
 -}
 
   
-type Power = Double
+
+--ToDo: Figure out a more logical place/module to put the AngleRadius functionality.
+
+{- |
+Combine an Angle and a Radius in a single datatype.
+Makes it easier to manipulate them, as opposed to when they are in separate lists.
+Afterwards the [Angle] [Radius] can be extracted using extract<Angle><Radius> functions,
+so that they can be used in the normal manner to generate [CornerPoints] through typical fucntions
+such as createBottomFaces or cylinder.
+
+Known uses:
+Expand shorter lists of [Radius] [Angle] so they can be matched up with a longer set of lists.
+For instance, adding the radii from an octagon, to the 36 angles from a typical scan, would
+require using filler values in the octagon list.
+An example of this is in: Examples.Scan.OpenBionicsDotComDesignWork
+-}
+data AngleRadius = AngleRadius
+                       { posDegree :: Angle,
+                         posRadius :: Radius
+                       }
+
+instance Show AngleRadius where
+  show (AngleRadius posDegree' posRadius') = (show posDegree') ++ " : " ++ (show posRadius')
+
+{- | Extract the [Angle] from a [AngleRadius]-}
+extractAngles :: [AngleRadius] -> [Angle]
+extractAngles anglesRadii =
+  extractAngles' (reverse anglesRadii) []
+
+extractAngles' :: [AngleRadius] -> [Angle] -> [Angle]
+extractAngles' (x:[]) angles =
+  posDegree x : angles
+
+extractAngles' (x:xs) angles =
+  extractAngles' xs (posDegree x : angles)
+
+{- | Extract the [Radius] from a [AngleRadius]-}
+extractRadii :: [AngleRadius] -> [Radius]
+extractRadii anglesRadii =
+  extractRadii' (reverse anglesRadii) []
+
+extractRadii' :: [AngleRadius] -> [Radius] -> [Radius]
+extractRadii'(x:[]) radii =
+  posRadius x : radii
+
+extractRadii' (x:xs) radii =
+  extractRadii' xs (posRadius x : radii)
+
