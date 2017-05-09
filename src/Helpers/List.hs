b71b594c70758e8948bd94228c2fc8f7cc13f179
Heath Weiss <heathweiss@hotmail.com>
2017-05-09 16:07:37 -0600

    added the splitAndReverseBackHalf function

--- src/Helpers/List.hs
+++ src/Helpers/List.hs
@@ -1,10 +1,12 @@
-module Helpers.List((++:), (++::)) where
+module Helpers.List((++:), (++::), SplitAtT(..), splitAndReverseBackHalf ) where
+
+type SplitAtT = Int
+
+infixl 3 ++:
+
 -- |Appends a list onto a list of lists.
 -- Known uses: building up a list of Faces to be zipped to list of Cornerpoints using (+++^) for createing stl output
 --give it a lower infix than ++
-infixl 3 ++:
-
-
 (++:) :: [[a]] -> [a] -> [[a]]
 multiList ++: singleList = reverse $ singleList : (reverse multiList)
 
@@ -18,3 +20,9 @@ multiList1 ++:: multiList2 = multiList1 ++ multiList2
 
 infixl 2 ++::
 
+
+splitAndReverseBackHalf :: SplitAtT -> [a] -> ([a],[a])
+splitAndReverseBackHalf splitListAt measurements =
+  let (front,back) = splitAt splitListAt measurements
+  in
+  (front, reverse back)
--- test/ListHelpersTest.hs
+++ test/ListHelpersTest.hs
@@ -1,11 +1,18 @@
 module ListHelpersTest(listHelperTestDo) where
 import Test.HUnit
-import Helpers.List((++:))
+import Helpers.List((++:), SplitAtT(..), splitAndReverseBackHalf )
 
 listHelperTestDo = do
+  putStrLn "\n\nListHelpersTest"
   runTestTT addTest
+  runTestTT splitTest
 
 addTest = TestCase $ assertEqual
   ("hello")
   ([[1,2,3],[4,5,6],[7,8,9]])
   ([[1,2,3], [4,5,6]] ++: [7,8,9])
+
+splitTest  = TestCase $ assertEqual
+  "split a valid list and reverse the 2nd half"
+  ([1,2,3],[6,5,4])
+  (splitAndReverseBackHalf 3 [1,2,3,4,5,6])
