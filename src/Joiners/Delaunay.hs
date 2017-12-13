32d9c4225f2cd74ce2b1b640b11b98c33a11f7f3
Heath Weiss <heathweiss@hotmail.com>
2017-12-13 13:18:46 -0700

    simplified getOrderedCpoint

--- src/Joiners/Delaunay.hs
+++ src/Joiners/Delaunay.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Joiners.Delaunay(delaunay) where
-
 {- |
 Join together 2,3,or 4 [CornerPoints].
 Eg: cutting a cylinder out of a scanned tread.
 -}
+module Joiners.Delaunay(delaunay) where
+
+
 
 import CornerPoints.CornerPoints(CornerPoints(..),(+++), center, (<-|->))
 
@@ -23,89 +24,66 @@ data Terminator =
 delaunay :: [CornerPoints] -> [CornerPoints] -> [CornerPoints] -> [CornerPoints] -> [CornerPoints]
 delaunay [] _ _ _ = [CornerPointsError "empty outer points passed into delaunay"]
 delaunay  _ [] [] [] = [CornerPointsError "at least 1 inner points required into delaunay"]
---find the first available to create the intial back line
---start with the 1st one for now. Will need better pattern matching
---and to see with array has the closest.
+--use the head of first list and head of another list to create the intial back <Face/line>.
 delaunay (i:is) (x:xs) [] [] =
   delaunay' (is) (xs) [] [] (i +++ x) ((i +++ x):[])
 
---Run 2 lists to the end of both lists.
---
+--Run (i:is) and at least 1 other list to the end of both lists.
+--(i:is) is the outer CornerPoints defining the shape into which all the others will be inserted.
 delaunay' :: [CornerPoints] -> [CornerPoints] -> [CornerPoints] -> [CornerPoints] ->  CornerPoints -> [CornerPoints] -> [CornerPoints] 
 delaunay'    (i:is)            (x:xs)            []                []                 currLine        cpointsJoined =
-  --temp to compile
-    --cpointsJoined 
   let
     avgDistance = getAvgDistance (i:is)            (x:xs)            []                []                 currLine 
     newCurrLine = buildLine
                     currLine
-                      (getLTECpoint
-                       (getAvgDistanceStatus
-                                       avgDistance
-                                       (calculateDistance (center currLine) (center i))
-                       )
-                       i)
-                      (getLTECpoint
-                       (getAvgDistanceStatus
-                          avgDistance
-                          (calculateDistance
-                            (center currLine)
-                            (center x)
-                          )
-                          
-                       )
-                       x
+                      (getOrderedCPoint
+                        avgDistance
+                        currLine
+                        i
                       )
+                      (getOrderedCPoint
+                        avgDistance
+                        currLine
+                        x
+                       )
                       (GTCPoint CornerPointsNothing)
                       (GTCPoint CornerPointsNothing)
   in
     delaunay' (removeHeadIfUsed (i:is) (F1 $ f1 newCurrLine)) (removeHeadIfUsed (x:xs) (B1 $ b1 newCurrLine)) [] [] newCurrLine (newCurrLine:cpointsJoined)
+
+
 
 delaunay'  (i:is) [] [] [] currLine cpointsJoined =
   let
     avgDistance = getAvgDistance (i:is)            []            []                []                 currLine 
     newCurrLine = buildLine
                     currLine
-                      (getLTECpoint
-                       (getAvgDistanceStatus
-                                       avgDistance
-                                       (calculateDistance (center currLine) (center i))
-                       )
-                       i)
+                      (getOrderedCPoint avgDistance currLine i)
                       (GTCPoint CornerPointsNothing)
                       (GTCPoint CornerPointsNothing)
                       (GTCPoint CornerPointsNothing)
   in
     delaunay' is [] [] [] newCurrLine (newCurrLine:cpointsJoined)
-  
 
 delaunay' [] (x:xs) [] [] currLine cpointsJoined =
   let
     avgDistance = getAvgDistance []            (x:xs)            []                []                 currLine 
     newCurrLine = buildLine
                     currLine
-                      (getLTECpoint
-                       (getAvgDistanceStatus
-                                       avgDistance
-                                       (Distance 100000000.0) --just give a big # for now
-                       )
-                       CornerPointsNothing) --just give it Nothing for now
-                      (getLTECpoint
-                       (getAvgDistanceStatus
-                          avgDistance
-                          (calculateDistance
-                            (center currLine)
-                            (center x)
-                          )
-                          
-                       )
-                       x
+                      (getOrderedCPoint
+                        avgDistance
+                        currLine 
+                        CornerPointsNothing) 
+                      (getOrderedCPoint
+                         avgDistance
+                         currLine
+                         x
                       )
                       (GTCPoint CornerPointsNothing)
                       (GTCPoint CornerPointsNothing)
   in
     delaunay' [] xs [] [] newCurrLine (newCurrLine:cpointsJoined)
-  
+
 delaunay'  [] [] [] [] _ cpointsJoined =  reverse cpointsJoined
 
 buildLine :: CornerPoints ->            OrderedCornerPoint -> OrderedCornerPoint -> OrderedCornerPoint -> OrderedCornerPoint -> CornerPoints
@@ -135,24 +113,45 @@ getAvgDistance    (i:is)            (x:xs)            []                []      
     iDist = calculateDistance (center i) (center currLine)
     xDist = calculateDistance (center x) (center currLine)
   in
+    case iDist of
+      (Distance d) ->
+        case xDist of
+          (Distance d') -> Distance $ (d + d')/2
+          NoDistance -> NoDistance
+      NoDistance -> NoDistance
+{-
+getAvgDistance :: [CornerPoints] -> [CornerPoints] -> [CornerPoints] -> [CornerPoints] -> CornerPoints -> Distance
+getAvgDistance    (i:is)            (x:xs)            []                []                currLine        =
+  let
+    iDist = calculateDistance (center i) (center currLine)
+    xDist = calculateDistance (center x) (center currLine)
+  in
+    
     Distance $ ((iDist^.distance) + (xDist^.distance))/2
+-}
 getAvgDistance    []            (x:xs)            []                []                currLine        =
   calculateDistance (center x) (center currLine)
 getAvgDistance  (i:is)  []          []                []                currLine        =
   calculateDistance (center i) (center currLine)
 
-getAvgDistanceStatus :: Distance    -> Distance -> Ordering
-getAvgDistanceStatus    avgDistance    (dist)
-  | (dist^.distance) > (avgDistance^.distance)  = GT
-  | (dist^.distance) < (avgDistance^.distance)  = LT
-  | otherwise = EQ
-  
-
-
-getLTECpoint :: Ordering -> CornerPoints -> OrderedCornerPoint
-getLTECpoint (LT) cpoint = LTCPoint cpoint
-getLTECpoint (EQ) cpoint = EQCPoint cpoint
-getLTECpoint (GT) cpoint = GTCPoint cpoint
+getOrderedCPoint :: Distance -> CornerPoints -> CornerPoints -> OrderedCornerPoint
+getOrderedCPoint    avgDist     advanceFromThisLine        orderThisCpoint =
+  let
+    myDist = calculateDistance
+             (center advanceFromThisLine)
+             (center orderThisCpoint)
+    getAvgDistanceStatus :: Distance    -> Distance -> Ordering
+    getAvgDistanceStatus _ NoDistance = GT
+    getAvgDistanceStatus NoDistance _ = GT
+    getAvgDistanceStatus    (Distance avgDistance)    (Distance dist)
+      | (dist) > (avgDistance)  = GT
+      | (dist) < (avgDistance)  = LT
+      | otherwise = EQ
+  in
+    case getAvgDistanceStatus avgDist myDist of
+      GT -> GTCPoint orderThisCpoint
+      LT -> LTCPoint orderThisCpoint
+      EQ -> EQCPoint orderThisCpoint
 
 removeHeadIfUsed :: [CornerPoints] -> CornerPoints -> [CornerPoints]
 removeHeadIfUsed (x:xs) cpoint =
--- src/Joiners/DelaunayViewer.hs
+++ src/Joiners/DelaunayViewer.hs
@@ -1,7 +1,8 @@
-module Joiners.DelaunayViewer() where
 {-
 Create some shapes to look at for building the Joiners.Delaunay module.
 -}
+
+module Joiners.DelaunayViewer() where
 
 import Primitives.Cylindrical.Walled(cylinder)
 
@@ -38,54 +39,71 @@ import Control.Monad.Except
 import Control.Monad.Writer (WriterT, tell, execWriterT)
 import Control.Monad.Reader
 
-anglesInner = ([Angle a | a <- [0,5..360]] )
-  --([Angle a | a <- [0,5..300]]++ [Angle 350] )
-innerCylinder = cylinder [Radius 40 | r <- [1..]] [Radius 50 | r <- [1..]] anglesInner (Point 0 0 0) 10
-innerBackBottomPoints = (extractB4 $ head innerCylinder) : (map (extractB1) $ {-tail-}  innerCylinder)
-
-anglesOuter = ([Angle a | a <- [0,10..360]])
-  --([Angle a | a <- [0,10..300]]++ [Angle 350] )
-outerCylinder = cylinder [Radius 60 | r <- [1..]] [Radius 80 | r <- [1..]] anglesOuter (Point 0 0 0) 10
-outerFrontBottomPoints = (extractF4 $ head outerCylinder) : (map (extractF1) $ {-tail-}  outerCylinder)
 
 
-d  = delaunay outerFrontBottomPoints innerBackBottomPoints [] []
+{-
+Cut a cylinder out of another cylinder.
+Done using only the bottom points: B1, B4, F1, F4.
+Inner cylinder is every 5 degrees, while outer is every 10 degrees.
+Uses the delaunay joiner to join the mismatched degrees.
 
-btmFaces = ((head d) +++> (tail d))
+Bottom faces are build from the bottom points, and then a top must be added to get the
+final walled cylinder.
+-}
+bottomPointsBuilder :: ExceptStackCornerPointsBuilder
+bottomPointsBuilder = do
+  let
+  {-----------------Build the 2 cylinders, and extract required bottom points to be joined as [BottomFace]-------------}
+  --Create the inner cylinder which will supply the [B<1/4>] used to build the inner wall.
+  innerBackBottomPoints <- buildCubePointsListSingle "innerBackBottomPoints"
+                            
+                            (let
+                                --build the inner cylinder
+                               angles = ([Angle a | a <- [0,5..360]] )
+                               cylinder' = cylinder [Radius 40 | r <- [1..]] [Radius 50 | r <- [1..]] angles (Point 0 0 0) 10
+                             in
+                              --extract all the Back points. Note that 1st the first cube needs B4 and B1 extracted.
+                              (extractB4 $ head cylinder') : (map (extractB1) cylinder')
+                            )
 
-cubes = (map (toTopFace) btmFaces)
-        |+++|
-        (btmFaces)
+  --Create the outer cylinder which supplies the [F<1/4>] used to build the outer wall.
+  outerFrontBottomPoints <- buildCubePointsListSingle "innerBackBottomPoints"
+                            (let
+                              --build the outer cylinder
+                               anglesOuter = ([Angle a | a <- [0,10..360]])
+                               outerCylinder = cylinder [Radius 60 | r <- [1..]] [Radius 80 | r <- [1..]] anglesOuter (Point 0 0 0) 10
+                             in
+                             --extract all the Front points. Note that the first cube needs F4 and F1 extracted.
+                              (extractF4 $ head outerCylinder) : (map (extractF1) $ {-tail-}  outerCylinder)
+                            )
 
-builder :: ExceptStackCornerPointsBuilder
-builder = do
-  innerBackBottomPoints' <- buildCubePointsListSingle "innerBackBottomPoints"
-                            innerBackBottomPoints
 
-  outerFrontBottomPoints' <- buildCubePointsListSingle "innerBackBottomPoints"
-                            outerFrontBottomPoints
 
-  delaunay'' <- buildCubePointsListSingle "delaunay"
+
+  
+  --Use delaunay joiner to join the <innerBack/outerFront>Points into a [Bottom<Left/Right>Line]
+  bottomLeftRightLines <- buildCubePointsListSingle "delaunay"
                 (delaunay
-                   outerFrontBottomPoints'
-                   innerBackBottomPoints'
+                   outerFrontBottomPoints
+                   innerBackBottomPoints
                    [] []
                 )
-
+  --create the [BottomFace] by: (head bottomLeftRightLines) +++> (tail bottomLeftRightLines)
   btmFaces <- buildCubePointsListSingle "btmFaces"
-           ((head delaunay'') +++> (tail delaunay'')
+           ((head bottomLeftRightLines) +++> (tail bottomLeftRightLines)
            )
 
+  --add top faces to the btmFaces to get cubes.
   cubes <- buildCubePointsListWithAdd "cubes"
            (map ((transposeZ (+10)) . toTopFace) btmFaces)
            (btmFaces)
 
   return cubes
 
-runBuilder :: IO ()
-runBuilder = do
+runBottomPointsBuilder :: IO ()
+runBottomPointsBuilder = do
   let
-    builder' = runExceptT builder
+    builder' = runExceptT bottomPointsBuilder
     cpoints = ((execState $ builder') [])
     valCpoints = ((evalState $ builder') [])
 
@@ -95,3 +113,17 @@ runBuilder = do
           liftIO $ putStrLn "output from Builder was good"
           liftIO $ writeStlToFile $ newStlShape "entire geox" $ [FacesAll | x <- [1..]] |+++^| (autoGenerateEachCube [] cpoints)
           liftIO $ putStrLn "stl should have been output"
+
+
+{-
+Cut 1 cylinder out of another by using outer faces of bigger cylinder and inner faces of inner cylinder.
+This will mean:
+outer cylinder:
+-extract front faces
+ -extract FrontRightLine of 1st cube, and [FrontLeftLine] of all cubes
+
+inner cylinder:
+-extract back faces
+ -extract BackRightLine of 1st cube, and [BackLeftLine] of all cubes.
+-}
+
