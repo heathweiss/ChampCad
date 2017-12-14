7d4e58a284e33052dc1fc58f38e1c90b46eaa6ab
Heath Weiss <heathweiss@hotmail.com>
2017-12-14 11:18:55 -0700

    added missing patterns.

--- src/CornerPoints/FaceExtraction.hs
+++ src/CornerPoints/FaceExtraction.hs
@@ -16,7 +16,7 @@ module CornerPoints.FaceExtraction (
  extractF1, extractF2, extractF3, extractF4,
  extractB1, extractB2, extractB3, extractB4
  ) where
-import CornerPoints.CornerPoints(CornerPoints(..))
+import CornerPoints.CornerPoints(CornerPoints(..), cpointType)
 import CornerPoints.Points(Point(..))
 
 extractBottomFace :: CornerPoints -> CornerPoints
@@ -54,6 +54,8 @@ extractBackBottomLine (BottomFace b1 f1 b4 f4) = BackBottomLine b1 b4
 
 extractBackLeftLine (BackFace b1 b2 b3 b4) = BackLeftLine b1 b2
 extractBackLeftLine (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = BackLeftLine b1 b2
+extractBackLeftLine (LeftFace b1 b2 f1 f2) = BackLeftLine b1 b2
+extractBackLeftLine unmatchedOrIllegal = CornerPointsError $ "unmatched or illegal extractBackLeftLine: " ++ ( cpointType unmatchedOrIllegal)
 
 extractBackFace :: CornerPoints -> CornerPoints
 extractBackFace (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = BackFace b1 b2 b3 b4
@@ -61,6 +63,8 @@ extractBackFace (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = BackFace b1 b2 b3 b4
 extractFrontLeftLine :: CornerPoints -> CornerPoints
 extractFrontLeftLine (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = FrontLeftLine f1 f2
 extractFrontLeftLine (FrontFace f1 f2 f3 f4) = FrontLeftLine f1 f2
+extractFrontLeftLine (LeftFace b1 b2 f1 f2) =  FrontLeftLine f1 f2 
+extractFrontLeftLine unmatchedOrIllegal = CornerPointsError $ "unmatched or illegal extractFrontLeftLine: " ++ ( cpointType unmatchedOrIllegal)
 
 extractBackRightLine :: CornerPoints -> CornerPoints
 extractBackRightLine (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = BackRightLine b3 b4
@@ -69,6 +73,7 @@ extractBackRightLine (BackFace b1 b2 b3 b4) = BackRightLine b3 b4
 extractFrontRightLine :: CornerPoints -> CornerPoints
 extractFrontRightLine (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = FrontRightLine f3 f4
 extractFrontRightLine (FrontFace f1 f2 f3 f4) = FrontRightLine f3 f4
+extractFrontRightLine unmatchedOrIllegal = CornerPointsError $ "unmatched or illegal extractFrontRightLine: " ++ ( cpointType unmatchedOrIllegal)
 
 extractLeftFace :: CornerPoints -> CornerPoints
 extractLeftFace (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = LeftFace b1 b2 f1 f2
@@ -78,6 +83,7 @@ extractF1 :: CornerPoints -> CornerPoints
 extractF1 (BottomFrontLine f1 f4) = F1 f1
 extractF1 (FrontLeftLine f1 f2) = F1 f1
 extractF1 (CubePoints f1 _ _ _ _ _ _ _) = F1 f1
+extractF1  (BottomLeftLine b1 f1) = F1 f1
 
 extractF2 :: CornerPoints -> CornerPoints
 extractF2 (FrontTopLine f2 f3) = F2 f2
@@ -97,6 +103,7 @@ extractF4 (CubePoints f1 f2 f3 f4 b1 b2 b3 b4) = F4 f4
 
 extractB1 :: CornerPoints -> CornerPoints
 extractB1 (CubePoints _ _ _ _ b1 _ _ _) = B1 b1
+extractB1 (BottomLeftLine b1 f1) = B1 b1
 
 extractB2 :: CornerPoints -> CornerPoints
 extractB2 (CubePoints _ _ _ _ _ b2 _ _) = B2 b2
@@ -106,3 +113,4 @@ extractB3 (CubePoints _ _ _ _ _ _ b3 _ ) = B3 b3
 
 extractB4 :: CornerPoints -> CornerPoints
 extractB4 (CubePoints _ _ _ _ _ _ _ b4 ) = B4 b4
+
